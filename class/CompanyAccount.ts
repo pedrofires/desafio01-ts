f6671f09401f90e20237b5397754621ee0c92152
Pedro Fires <pedrof.fires@gmail.com>
2024-11-10 19:40:21 -0300

    feat: companyAccount now can make getLoan

--- class/CompanyAccount.ts
+++ class/CompanyAccount.ts
@@ -2,11 +2,15 @@ import { DioAccount } from "./DioAccount"
 
 export class CompanyAccount extends DioAccount {
 
-  constructor(name: string, accountNumber: number){
+  constructor(name: string, accountNumber: number) {
     super(name, accountNumber)
   }
 
-  getLoan = (): void => {
+  getLoan = (amount: number): void => {
+    if (!this.validateStatus()) return;
+
+    const newBalance = this.getBalance() + amount;
+    this.deposit(newBalance + amount);
     console.log('Voce pegou um empréstimo')
   }
 }
